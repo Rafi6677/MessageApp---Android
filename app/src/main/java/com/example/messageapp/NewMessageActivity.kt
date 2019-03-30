1152512596a1fe88f4a94f2a6e4803daef54f4cb
Rafał Giemza <rafalgiemza6677@gmail.com>
2019-03-30 23:45:38 +0100

    NewMessageActivity adapter prototype

--- app/src/main/java/com/example/messageapp/NewMessageActivity.kt
+++ app/src/main/java/com/example/messageapp/NewMessageActivity.kt
@@ -2,6 +2,11 @@ package com.example.messageapp
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.widget.RecyclerView
+import com.xwray.groupie.GroupAdapter
+import com.xwray.groupie.Item
+import com.xwray.groupie.ViewHolder
+import kotlinx.android.synthetic.main.activity_new_message.*
 
 class NewMessageActivity : AppCompatActivity() {
 
@@ -9,6 +14,33 @@ class NewMessageActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_new_message)
 
-        supportActionBar?.title = "Wiadomość do"
+        supportActionBar?.title = "Wiadomość do:"
+
+        val adapter = GroupAdapter<ViewHolder>()
+
+        adapter.add(UserItem())
+        adapter.add(UserItem())
+        adapter.add(UserItem())
+
+        recyclerViewNewMessage.adapter = adapter
+
     }
 }
+
+class UserItem: Item<ViewHolder>() {
+    override fun bind(viewHolder: ViewHolder, position: Int) {
+
+    }
+
+    override fun getLayout(): Int {
+        return R.layout.user_row_new_message
+    }
+}
+
+/*
+
+class CustomAdapter: RecyclerView.Adapter<RecyclerView.ViewHolder> {
+
+}
+
+*/
